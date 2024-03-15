606e6ac07469e4ddce640dea5a7ce208b3763725
Maestro1982 <kris.tote@telenet.be>
2024-03-15 14:23:11 +0100

    modify ProtectedRoute.tsx in frontend src auth folder

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -3,7 +3,17 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth0();
-  return isAuthenticated ? <Outlet /> : <Navigate to='/' replace />;
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <Navigate to='/' replace />;
 };
+
 export default ProtectedRoute;
