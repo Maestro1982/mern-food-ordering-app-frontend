4495da3a2d1dfe27ba8359d906894f5df4a90e62
Maestro1982 <kris.tote@telenet.be>
2024-03-22 15:06:22 +0100

    modify Homepage2tsx in frontend src pages folder, create Searchbar.tsx in frontend src components folder, SearchPage.tsx in frontend src pages folder

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
 import landingImage from '@/assets/landing.png';
 import appDownloadImage from '@/assets/appDownload.png';
 
+import Searchbar, { SearchForm } from '@/components/Searchbar';
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className='flex flex-col gap-12'>
-      <div className='flex flex-col bg-white rounded-lg shadow-md py-8 gap-5 text-center -mt-16'>
+      <div className='flex flex-col bg-white rounded-lg shadow-md py-8 gap-5 text-center -mt-16 md:px-32'>
         <h1 className='text-2xl md:text-5xl px-1 font-bold tracking-tight text-orange-500'>
           Tuck into a takeaway today
         </h1>
         <span className='text-xl'>Food is just a click away!</span>
+        <Searchbar
+          placeholder='Search by City or Town'
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className='grid md:grid-cols-2 gap-5'>
         <img src={landingImage} alt='Landing image' />
